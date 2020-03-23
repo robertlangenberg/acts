5db741b4a79a58dbc1cbea6b22ecd954e23b09a8
Paul Gessinger <hello@paulgessinger.com>
2020-03-23 17:09:17 +0100

    format fix

--- Tests/CommonHelpers/Acts/Tests/CommonHelpers/BenchmarkTools.hpp
+++ Tests/CommonHelpers/Acts/Tests/CommonHelpers/BenchmarkTools.hpp
@@ -360,9 +360,11 @@ using call_without_input_t = decltype(std::declval<T>()());
 // this specialization will be selected...
 template <typename Callable, typename Input = void>
 struct MicroBenchmarkIter {
-  constexpr static bool is_callable = concept::exists<call_with_input_t, Callable, Input>;
+  constexpr static bool is_callable =
+      concept ::exists<call_with_input_t, Callable, Input>;
   static inline void iter(const Callable& iteration, const Input* input) {
-    static_assert(is_callable, "Gave callable that is not callable without input");
+    static_assert(is_callable,
+                  "Gave callable that is not callable without input");
     if constexpr (is_callable) {
       using Result = std::invoke_result_t<Callable, const Input&>;
       MicroBenchmarkIterImpl<Callable, Input, Result>::iter(iteration, *input);
@@ -374,17 +376,17 @@ struct MicroBenchmarkIter {
 // picked instead of the one above...
 template <typename Callable>
 struct MicroBenchmarkIter<Callable, void> {
-  constexpr static bool is_callable = concept::exists<call_without_input_t, Callable>;
+  constexpr static bool is_callable =
+      concept ::exists<call_without_input_t, Callable>;
 
   static inline void iter(const Callable& iteration, const void* = nullptr) {
     static_assert(is_callable, "Gave callable that is not callable with input");
-    if constexpr(is_callable) {
-    using Result = std::invoke_result_t<Callable>;
-    MicroBenchmarkIterImpl<Callable, void, Result>::iter(iteration);
+    if constexpr (is_callable) {
+      using Result = std::invoke_result_t<Callable>;
+      MicroBenchmarkIterImpl<Callable, void, Result>::iter(iteration);
     }
   }
 };
-
 
 // Common logic between iteration-based and data-based microBenchmark
 template <typename Callable>
